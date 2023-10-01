0657a0e2d97d3e3cacca8e3b7e7bf50ab23a46d1
QuanZzz <zhouquaneric@gmail.com>
2023-09-30 21:48:01 -0400

    format the date

--- src/routes/CustomersTable.js
+++ src/routes/CustomersTable.js
@@ -3,11 +3,24 @@ import { usersInfo, usersInfoColumns } from "../utils/constants/usersInfo";
 import { CUSTOMERS_TABLE_ROWS_NUMBERS } from "../utils/constants/constants";
 
 export default function CustomersTable() {
+  const formattedUsersInfo = usersInfo.map(user => {
+    const createdAt = new Date(user.created_at);
+    const year = createdAt.getFullYear();
+    const month = String(createdAt.getMonth() + 1).padStart(2, '0');
+    const day = String(createdAt.getDate()).padStart(2, '0');
+    const hours = String(createdAt.getHours()).padStart(2, '0');
+    const minutes = String(createdAt.getMinutes()).padStart(2, '0');
+    const seconds = String(createdAt.getSeconds()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    return { ...user, created_at: formattedDate };
+  });
+  
   return(
     <div>
       <DataTable
         header="Customers Information"
-        initialData={usersInfo}
+        initialData={formattedUsersInfo}
         columns={usersInfoColumns}
         rowsSizes={CUSTOMERS_TABLE_ROWS_NUMBERS}
       />
